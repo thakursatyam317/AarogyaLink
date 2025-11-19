08c59f0163999103419907bb46e1bd3fd3e77b3f
Satyam Thakur <thakursatyam317@gmail.com>
2025-11-19 19:08:57 +0530

    feat: enhance Notifiation component with structured layout and content

--- client/src/pages/dashboard/doctor/Notifiation.jsx
+++ client/src/pages/dashboard/doctor/Notifiation.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Notifiation = () => {
   return (
-    <div>Notifiation</div>
+    <>
+    <div>
+      <div>
+        <h1>Notification</h1>
+        <div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <h1>Satyam Thakur</h1>
+              <p>Doctor Appointment</p>
+              <p>10th Aug, 2023 | 10:00 AM</p>
+              
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    
+    </>
   )
 }
 
